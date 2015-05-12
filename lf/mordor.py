19b59192360d429de9e08a916ed123c810d929c3
Lauren Anderson <l.sonofanders@gmail.com>
2015-05-12 14:01:12 -0500

    mordor update

--- lf/mordor.py
+++ lf/mordor.py
@@ -1,8 +1,11 @@
 import pynbody
-import matplotlib.pyplot as p
+import matplotlib.pyplot as plt
 import numpy as np
 import customCmap as cc
 import pdb
+import os
+
+threads = 2
 
 #color palette 1
 color1 = (5, 16, 20)
@@ -52,33 +55,52 @@ cp5 = [color1, color2, color3, color4, color5, color6]
 
 
 colors = cp5
-position = [0, 0.1, 0.2, 0.6, 0.7, 1]
+
+"""
+virden = 25486.1655
+logvirden = np.log10(virden/50.)
+logvirden01 = np.log10(virden/200.)
+logvirden001 = np.log10(virden/1000.)
+vmin = 1
+vmax = 5
+posvir = (logvirden-vmin)/(vmax - vmin)
+posvir01 = (logvirden01-vmin)/(vmax - vmin)
+posvir001 = (logvirden001-vmin)/(vmax - vmin)
+print posvir, posvir01, posvir001
+position = [0, 0.05, posvir001, posvir01, posvir, 1]
+"""
+
+virden = 25486.1655
+logvirden1 = np.log10(virden/20.)
+logvirden2 = np.log10(virden/200.)
+logvirden3 = np.log10(virden/1000.)
+logvirden4 = np.log10(virden/2000.)
+vmin = 0.5
+vmax = 5
+posvir1 = (logvirden1-vmin)/(vmax - vmin)
+posvir2 = (logvirden2-vmin)/(vmax - vmin)
+posvir3 = (logvirden3-vmin)/(vmax - vmin)
+print posvir1, posvir2, posvir3
+#position = [0, 0.1, 0.2, 0.5, 0.7, 1]
+position = [0, 0.05, posvir3, posvir2, posvir1, 1]
 cmap = cc.make_cmap(colors, bit=True, position=position)
 
-filename = '/scratch/sciteam/lmanders/cosmo8.33PLK.256g2bwK1C52/cosmo8.33PLK.256g2bwK1C52.002763'
+dir = '/scratch/sciteam/lmanders/cosmo8.33PLK.256g2bwK1C52/'
+filename = dir + 'cosmo8.33PLK.256g2bwK1C52.002763'
 qty = 'rho'
-threads = 8
-resolution = 1000
+
+resolution = 4000
 n = resolution
 nz = 200
 width = 6000
-vmin = 1
-vmax = 4.5
 
 prefix = filename.split('/')[-1].split('.')[0]
 print prefix
 
-sim = pynbody.load(filename)
-sim.physical_units()
-p.gray()
-
+#p.gray()
 #maybe
 #h = sim.halos()
 #pynbody.angmom.sideon(h[1])
-
-
-sim.gas['smooth'] = (sim.gas['mass']/sim.gas['rho'])**(1,3)
-
 #my_slice = sim.gas[pynbody.filt.BandPass('z',-500,500)]
 #pynbody.plot.sph.image(my_slice, width=width, qty=qty, av_z=True, cmap=cmap, threaded=threads, resolution=resolution)
 #p.imshow(np.log10(grid[:,:,50]), cmap=cmap, vmin=-1, vmax=3)
@@ -87,15 +109,27 @@ sim.gas['smooth'] = (sim.gas['mass']/sim.gas['rho'])**(1,3)
 
 
 try: 
-    grid = np.load('grid.' + prefix + '.' + str(resolution) + '.' + qty +'.npy')
+    print 'trying to load grid'
+    grid = np.load(dir + 'grid.' + prefix + '.' + str(resolution) + '.' + qty +'.new.npy')
 except IOError:
     print 'building grid'
+    sim = pynbody.load(filename)
+    sim.physical_units()
+    sim.gas['smooth'] = (sim.gas['mass']/sim.gas['rho'])**(1,3)
     grid = pynbody.sph.to_3d_grid(sim.gas, qty = qty, nx=n, ny=n, nz=nz, threaded=threads)
-    np.save('grid.' + prefix + '.' + str(resolution) + '.' + qty, grid)
+    np.save(dir + 'grid.' + prefix + '.' + str(resolution) + '.' + qty + '.new', grid)
+
+grid[np.where(grid == 0)] = abs(grid[np.where(abs(grid != 0))]).min()
+plt.imshow(np.log10(grid[:,:,193]), cmap=cmap, vmin=vmin, vmax=vmax)
+plt.show()
+pdb.set_trace()
+print 'creating images'
+if not os.path.exists(dir + 'moviepngs'):
+    os.makedirs(dir + 'moviepngs')
 
 for i in range(nz):
-    p.imsave('moviepngs/' + prefix + '.' + '{0:03d}'.format(i) + '.png', 
-             np.log10(grid[:,:,i]), vmin=vmin, vmax=vmax, cmap=cmap)
+    plt.imsave(dir + 'moviepngs/' + prefix + '.' + '{0:03d}'.format(i) + '.png', 
+               np.log10(grid[:,:,i]), vmin=vmin, vmax=vmax, cmap=cmap)
 
 """
 pynbody.plot.sph.image(dmy_slice, width=1000, qty='temp', av_z=True)
